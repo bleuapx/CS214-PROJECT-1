093873ab5463f878dc198325f7e3d830dbf2055d
bleuapx <123344021+bleuapx@users.noreply.github.com>
2023-03-23 22:51:35 +0000

    Added Lizard and Spock as moves.

--- RPSLS.java
+++ RPSLS.java
@@ -5,11 +5,11 @@ class Main {
    
       //Get the users input
       Scanner in = new Scanner(System.in);
-      System.out.println("Enter your move. Type in rock, paper, scissors, lizard, or spock: ");
+      System.out.println("Enter your move. Type in rock, paper, scissors, lizard, or spock. If you want to exit the game, type in quit. ");
       String myMove = in.nextLine();
       
       //Verify that my move is valid
-      if(!myMove.equals("rock") && !myMove.equals("paper") && !myMove.equals("scissors")) {
+      if(!myMove.equals("rock") && !myMove.equals("paper") && !myMove.equals("scissors") && !myMove.equals("lizard") && !myMove.equals("spock")) {
       System.out.println("Your move isn't valid!");
       } else {
       
@@ -20,15 +20,19 @@ class Main {
        opponentMove = "rock";
       } else if(rand == 1) {
         opponentMove = "paper";
-      } else {
+      } else if(rand == 2) {
         opponentMove = "scissors";
+      } else if(rand == 3) {
+        opponentMove = "lizard";
+      } else {
+      opponentMove = "spock";
       }
       System.out.println("Opponent move: " + opponentMove);
 
       //Calculate if the user won, lost, or tied
       if(myMove.equals(opponentMove)) {
-        System.out.println("You tied!:");
-      } else if((myMove.equals("rock") && opponentMove.equals("scissors")) || (myMove.equals("scissors") && opponentMove.equals("paper")) || (myMove.equals("paper") && opponentMove.equals("rock"))) {
+        System.out.println("You tied!");
+      } else if((myMove.equals("rock") && opponentMove.equals("scissors")) || (myMove.equals("scissors") && opponentMove.equals("paper")) || (myMove.equals("paper") && opponentMove.equals("rock")) || (myMove.equals("rock") && opponentMove.equals("lizard")) || (myMove.equals("lizard") && opponentMove.equals("paper")) || (myMove.equals("paper") && opponentMove.equals("spock")) || (myMove.equals("spock") && opponentMove.equals("scissors")) || (myMove.equals("scissors") && opponentMove.equals("lizard")) || (myMove.equals("lizard") && opponentMove.equals("spock")) || (myMove.equals("spock") && opponentMove.equals("rock"))) {
         System.out.println("You won!");
       } else {
         System.out.println("You lost!");
